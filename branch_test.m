df4b2cdaa03bda85482821648e4d58df0c011127
Robert Pritchett <repritchett92@gmail.com>
2017-03-08 13:08:26 -0500

    modify branch_test file

---
+++ branch_test.m
@@ -0,0 +1,9 @@
+% branch_test.m
+%
+% Test of git branching feature in Matlab
+
+% Print text
+disp('This is a test of the branching tool')
+
+% Add message during branch edits
+disp('This text was added while in a branch')
